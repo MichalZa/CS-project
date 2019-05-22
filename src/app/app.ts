68013fb98362b6bfd5c9ad56cf1416772baea4ee
Michał <michalzarow@gmail.com>
2019-05-22 02:36:45 +0200

    clean

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,9 +10,6 @@ import config from './config';
 import ErrorHandler from './../common/error/ErrorHandler';
 import logger from './logger';
 import * as helmet  from 'helmet';
-import AuthController from './../controller/AuthController';
-import ProjectController from './../controller/ProjectController';
-import CommentController from './../controller/CommentController';
 
 export default async (): Promise<express.Application> => {
 
@@ -32,7 +29,6 @@ export default async (): Promise<express.Application> => {
     });
 
     await Promise.all([database(), redis()]).catch((e) => {
-        console.log(e);
         errorHandler.handle(e, false);
     });
 
@@ -47,7 +43,7 @@ export default async (): Promise<express.Application> => {
     
     return useExpressServer(app, {
         routePrefix: '/api',
-        controllers: [AuthController, ProjectController, CommentController],
+        controllers: [__dirname + '/../controller/*.js'],
         middlewares: [__dirname + '/../middleware/*.js'],
         currentUserChecker: async (action: Action) => {
             return action.request.currentUser;
